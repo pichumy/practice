d0bbfc48f4ee13b8f28a4d19544baeb0d9e1490b
Dan <pichumy@gmail.com>
2018-07-18 16:44:00 -0700

    Evaluate does not deal well with negative numbers.

---
+++ evaluate.js
@@ -0,0 +1,95 @@
+// Receive a string of mathematical expressions
+// Parse the string and conduct the expression with order of operations
+// EXAMPLE: "3+6/4*7^2"
+// Currently does not deal well with negative numbers... bleh
+
+const evaluate = (input) => {
+  let string = input.repeat(1);
+  // first evaluate for exponents
+  while(string.indexOf('^') !== -1){
+    string = calculate(string, '^');
+    console.log(string);
+  }
+  // Evaluate multiplication and division next.
+  while(string.indexOf('*') !== -1 || string.indexOf('/') !== -1){
+    let mult = string.indexOf('*');
+    let div = string.indexOf('/');
+    if(mult > div && div !== -1){
+      string = calculate(string, '/');
+    }else if(mult !== -1){
+      string = calculate(string, '*');
+    }else{
+      string = calclate(string, '/');
+    }
+    console.log(string);
+  }
+
+  while(string.indexOf('+') !== -1){
+    string = calculate(string, '+');
+    console.log(string);
+  }
+  while(string.indexOf('-') !== -1){
+    string = calculate(string, '-');
+    console.log(string);
+  }
+  return parseInt(string);
+}
+
+const calculate = (string, operation) => {
+  let index = string.indexOf(operation);
+  let left_value = parse_left(string, index - 1);
+  let right_value = parse_right(string, index + 1);
+  let start_point = string.indexOf(left_value);
+  let end_point = string.indexOf(right_value) + right_value.length - 1;
+  let new_string = string.slice(0, start_point);
+  let end_string = string.slice(end_point + 1);
+  let new_value = null;
+  switch (operation){
+    case '^':
+      console.log('exponent');
+      new_value = Math.pow(parseInt(left_value), parseInt(right_value)).toString();
+      break;
+    case '*':
+      console.log('mult');
+      new_value = (parseInt(left_value) * parseInt(right_value)).toString();
+      break;
+    case '/':
+      console.log('div');
+      new_value = Math.floor(parseInt(left_value) / parseInt(right_value)).toString();
+      break;
+    case '+':
+      console.log('add');
+      new_value = (parseInt(left_value) + parseInt(right_value)).toString();
+      break;
+    case '-':
+      console.log('subtract');
+      new_value = (parseInt(left_value) - parseInt(right_value)).toString();
+      break;
+    default: console.log("error, unrecognized operator");
+  }
+  return new_string + new_value + end_string;
+}
+
+const parse_left = (array, index) => {
+  if(index === 0){
+    return `${array[index]}`;
+  }
+  if(isNaN(array[index])){
+    return '';
+  }else{
+    return parse_left(array, index - 1) + `${array[index]}`;
+  }
+}
+const parse_right = (array, index) => {
+  if(index === array.length - 1){
+    return `${array[index]}`;
+  }
+  if(isNaN(array[index])){
+    return '';
+  }else{
+    return `${array[index]}` + parse_right(array, index + 1);
+  }
+}
+
+let string = "3+6-8/4*7^2";
+console.log(evaluate(string));
---
+++ heap.js
@@ -0,0 +1,55 @@
+class MaxHeap {
+  constructor(store = []){
+    this.store = store;
+  }
+  count(){
+    return this.store.length;
+  }
+  insert(value){
+    this.store.push(value);
+    heapify_up(this.store.length - 1);
+  }
+  // [0, 1, 2, 3, 4, 5, 6]
+  // index[1] has children 3,4
+  // index[2] has children 5,6
+  children(index){
+    let indices = [];
+    let length = count();
+    let index1 = index * 2 + 1;
+    let index2 = index * 2 + 2;
+    if(index2 < length){
+      indices.push(index2);
+      indices.push(index1);
+    }else if(index1 < length){
+      indices.push(index1)
+    }
+    return indices;
+  }
+  // index[1] has parent [0]
+  // index[2] has parent [0]
+  // index[3] has parent [1]
+  // index[4] has parent [1]
+  // index[5] has parent [2]
+  // index[6] has parent [2]
+  parent(index){
+    // root has no parents
+    if(index === 0){
+      return null;
+    }
+    if(index % 2 === 0){
+      return Math.floor((index - 1) / 2);
+    }else{
+      return Math.floor(index / 2);
+    }
+  }
+  heapify_up(index){
+    let parent_idx = parent(index);
+    if(this.store[index] > this.store[parent_idx]){
+      [this.store[index], this.store[parent_idx]] = [this.store[parent_idx], this.store[index]];
+      heapify_up(parent_idx);
+    }
+  }
+  heapify_down(index){
+    let children = children(index);
+  }
+}
